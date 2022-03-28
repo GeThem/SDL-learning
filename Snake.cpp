0bb78b760ed22eeeb5fd187a0e21b11d3735fa1b
GeThem <99418179+GeThem@users.noreply.github.com>
2022-03-28 17:38:46 +0300

    -

--- Snake.cpp
+++ Snake.cpp
@@ -1,6 +1,8 @@
 #include <SDL.h>
 #include <SDL_image.h>
-#include <iostream>
+#include <time.h>
+#include <random>
+#include <malloc.h>
 #include "SDL_default.h"
 
 int win_w = 800, win_h = 600;
@@ -8,21 +10,36 @@ int win_w = 800, win_h = 600;
 struct Snake
 {
 	int length = 0;
-	SDL_Rect body[300] = {};
+	SDL_Rect* body = NULL;
 	Uint8 direction = 0, moves = 0;
 	int tile_size = 0;
 	bool alive = false;
 };
 
-void snake_init(Snake& self)
+bool colliderect(SDL_Rect self, SDL_Rect rect)
 {
-	self.length = 3;
+	return self.x == rect.x and self.y == rect.y;
+}
+
+int collidelist(SDL_Rect self, SDL_Rect* list, int start, int end)
+{
+	for (int i = start; i < end; i++)
+		if (self.x == list[i].x and self.y == list[i].y)
+			return i;
+	return -1;
+}
+
+void snake_init(Snake& self, int* pg_size, int tile_size = 40, int length = 2)
+{
 	self.alive = true;
+	self.length = length;
+	self.tile_size = tile_size;
+	self.body = (SDL_Rect*)realloc(self.body, sizeof(SDL_Rect) * pg_size[0] * pg_size[1]);
 	self.direction = self.moves =  0;
-	self.tile_size = 40;
-	self.body[0] = { 11 * self.tile_size, 7 * self.tile_size, self.tile_size, self.tile_size };
-	self.body[1] = { 10 * self.tile_size, 7 * self.tile_size, self.tile_size, self.tile_size };
-	self.body[2] = { 9 * self.tile_size, 7 * self.tile_size, self.tile_size, self.tile_size };
+	if (self.body == NULL)
+		quit();
+	self.body[0] = { (pg_size[0] / 2) * self.tile_size,  pg_size[1] / 2 * self.tile_size, self.tile_size, self.tile_size };
+	self.body[1] = { (pg_size[0] / 2 - 1) * self.tile_size, pg_size[1] / 2 * self.tile_size, self.tile_size, self.tile_size };
 }
 
 void snake_move(Snake& self)
@@ -64,14 +81,6 @@ void snake_move(Snake& self)
 	}
 }
 
-int snake_collide_self(Snake self)
-{
-	for (int i = 3; i < self.length; i++)
-		if (self.body[i].x == self.body[0].x and self.body[i].y == self.body[0].y)
-			return i;
-	return 0;
-}
-
 void snake_draw(Snake self)
 {
 	double diff = 155.0 / self.length;
@@ -88,14 +97,12 @@ void apple_draw(SDL_Rect self)
 	SDL_RenderFillRect(ren, &self);
 }
 
-SDL_Rect apple_spawn(int* pg_size, int tile_size)
+SDL_Rect apple_spawn(int* pg_size, int tile_size, Snake snake)
 {
-	return { rand() % pg_size[0] * tile_size, rand() % pg_size[1] * tile_size, tile_size, tile_size };
-}
-
-bool colliderect(SDL_Rect self, SDL_Rect rect)
-{
-	return self.x == rect.x and self.y == rect.y;
+	SDL_Rect apple = { rand() % pg_size[0] * tile_size, rand() % pg_size[1] * tile_size, tile_size, tile_size };
+	if (collidelist(apple, snake.body, 0, snake.length) != -1)
+		apple = { snake.body[snake.length - 1].x, snake.body[snake.length - 1].y, tile_size, tile_size };
+	return apple;
 }
 
 int main(int argc, char** args)
@@ -103,12 +110,15 @@ int main(int argc, char** args)
 	init();
 	display_init(win_w, win_h, "Snake");
 
+	int tile_size = 40;
+	int pg_size[] = { win_w / tile_size, win_h / tile_size };
+	
 	Snake snake;
-	snake_init(snake);
+	snake_init(snake, pg_size, tile_size);
 
-	int pg_size[] = { win_w / snake.tile_size - 1, win_h / snake.tile_size - 1};
+
 	srand(time(0));
-	SDL_Rect apple = apple_spawn(pg_size, snake.tile_size);
+	SDL_Rect apple = apple_spawn(pg_size, tile_size, snake);
 
 	SDL_Event ev;
 	const Uint8* kbstate = SDL_GetKeyboardState(NULL);
@@ -127,8 +137,8 @@ int main(int argc, char** args)
 					switch (ev.key.keysym.scancode)
 					{
 					case SDL_SCANCODE_R:
-						snake_init(snake);
-						apple = apple_spawn(pg_size, snake.tile_size);
+						snake_init(snake, pg_size, tile_size);
+						apple = apple_spawn(pg_size, tile_size, snake);
 						break;
 					case SDL_SCANCODE_ESCAPE:
 						quit();
@@ -147,17 +157,18 @@ int main(int argc, char** args)
 		{
 			if (colliderect(apple, snake.body[0]))
 			{
-				apple = apple_spawn(pg_size, snake.tile_size);
+				apple = apple_spawn(pg_size, tile_size, snake);
 				snake.length++;
 			}
 
 			snake_move(snake);
 
-			if (snake.body[0].x < 0 or snake.body[0].x >= win_w or snake.body[0].y < 0 or snake.body[0].y >= win_h)
+			if (snake.body[0].x < 0 or snake.body[0].x >= win_w or snake.body[0].y < 0 or snake.body[0].y >= win_h or
+				snake.length == pg_size[0] * pg_size[1])
 				snake.alive = false;
 
-			int collides = snake_collide_self(snake);
-			if (collides)
+			int collides = collidelist(snake.body[0], snake.body, 3, snake.length);
+			if (collides != -1)
 				snake.length = collides;
 
 		}
